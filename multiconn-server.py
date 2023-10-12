52902d0541236ccf32516b1754d52506d43a3dde
shugun2000 <shugun3000@gmail.com>
2023-10-12 13:54:03 +0700

    Changed

--- multiconn-server.py
+++ multiconn-server.py
@@ -3,72 +3,62 @@ import socket
 import selectors
 import types
 
-sel = selectors.DefaultSelector()
-messages = [b"Message 1 from client.", b"Message 2 from client."]
+def start_server(host, port):
+    sel = selectors.DefaultSelector()
+    messages = [b"Message 1 from client.", b"Message 2 from client."]
 
-def start_connections(host, port, num_conns):
-    server_addr = (host, port)
-    for i in range(0, num_conns):
-        connid = i + 1
-        print(f"Starting connection {connid} to {server_addr}")
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setblocking(False)
-        sock.connect_ex(server_addr)
+    def accept_wrapper(sock):
+        conn, addr = sock.accept()
+        print(f"Accepted connection from {addr}")
+        conn.setblocking(False)
+        data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        data = types.SimpleNamespace(
-        connid=connid,
-msg_total=sum(len(m) for m in messages),
-recv_total=0,
-messages=messages.copy(),
-outb=b"",
-)
-        sel.register(sock, events, data=data)
+        sel.register(conn, events, data=data)
 
+    def service_connection(key, mask):
+        sock = key.fileobj
+        data = key.data
 
-host, port = sys.argv[1], int(sys.argv[2])
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
-lsock.listen()
-print(f"Listening on {(host, port)}")
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
-
-def accept_wrapper(sock):
-    conn, addr = sock.accept()  # This should be ready to read data
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print(f"Closing connection to {data.addr}")
-            sel.unregister(sock)
-            sock.close()
-
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print("Echoing", repr(data.outb), "to", data.addr)
-            sent = sock.send(data.outb)
-            data.outb = data.outb[sent:]
-
-try:
-    while True:
-        events = sel.select()
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
+        if mask & selectors.EVENT_READ:
+            recv_data = sock.recv(1024)
+            if recv_data:
+                data.outb += recv_data
+                print(f"Received {recv_data!r} from {data.addr}")
             else:
-                service_connection(key, mask)
-except KeyboardInterrupt:
-    print("Caught keyboard interrupt, exiting")
-finally:
-    sel.close()
+                print(f"Closing connection to {data.addr}")
+                sel.unregister(sock)
+                sock.close()
+
+        if mask & selectors.EVENT_WRITE:
+            if data.outb:
+                print(f"Sending {data.outb!r} to {data.addr}")
+                sent = sock.send(data.outb)
+                data.outb = data.outb[sent:]
+
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.bind((host, port))
+    lsock.listen()
+    print(f"Listening on {(host, port)}")
+    lsock.setblocking(False)
+    sel.register(lsock, selectors.EVENT_READ, data=None)
+
+    try:
+        while True:
+            events = sel.select()
+            for key, mask in events:
+                if key.data is None:
+                    accept_wrapper(key.fileobj)
+                else:
+                    service_connection(key, mask)
+    except KeyboardInterrupt:
+        print("Caught keyboard interrupt, exiting")
+    finally:
+        sel.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <host> <port>")
+    else:
+        host, port = sys.argv[1], int(sys.argv[2])
+        for _ in range(5):  # Start 5 client connections
+            start_server(host, port)
