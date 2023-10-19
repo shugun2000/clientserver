208fa7d10c3628824d1be4d2897b22411615e3d3
shugun2000 <shugun3000@gmail.com>
2023-10-19 10:44:44 +0700

    added multi client

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # clientserver
+Commands
+
+Start Server.py
+then go in command prompt or terminal
+
+type "python client.py" 
+sad
+
+or
+
+type "python multiconn-threaded.py
---
+++ multiconn-threaded.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+def threaded(c):
+    while True:
+        data = c.recv(1024)
+        if not data:
+            print('Bye')
+            break
+
+        data = data[::-1]
+
+        c.send(data)
+
+    c.close()
+
+def Main():
+    host = '10.1.2.138'  # The host should be a string
+    port = 65432
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print("Socket bound to port", port)
+    s.listen(5)
+    print("Socket is listening")
+    
+    while True:
+        c, addr = s.accept()
+        print('Connected to:', addr[0], ':', addr[1])
+        threading.Thread(target=threaded, args=(c,)).start()
+
+if __name__ == '__main__':
+    Main()
