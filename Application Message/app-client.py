86f4f6e8ca5103eb48674896d04f4d43915ecde0
shugun2000 <shugun3000@gmail.com>
2023-11-01 12:05:41 +0700

    32

--- Application Message/app-client.py
+++ Application Message/app-client.py
@@ -1,39 +1,24 @@
 import sys
 import socket
-import selectors
-import types
-import traceback
-import libclient  
-
-sel = selectors.DefaultSelector() 
+import threading
+import libclient
 
 def start_connection(host, port, request):
     addr = (host, port)
     print(f"Starting connection to {addr}")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setblocking(False)  
+    sock.setblocking(False)
     sock.connect_ex(addr)
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    message = libclient.Message(sel, sock, addr, request)  
-    sel.register(sock, events, data=message)
+    message = libclient.Message(sock, addr, request)
+    message.start()  
+    
+if __name__ == '__main__':
+    host = '127.0.0.1'
+    port = 65432
+    request = b"binary test"
 
-try:
-    while True:
-        events = sel.select(timeout=1)
-        for key, mask in events:
-            message = key.data
-            try:
-                message.process_events(mask)
-            except Exception:
-                print(
-                    f"main: Error: Exception for {message.addr}:\n"
-                    f"{traceback.format_exc()}"
-                )
-                message.close()
-
-        if not sel.get_map():
-            break
-except KeyboardInterrupt:
-    print("Caught a keyboard interrupt, exiting")
-finally:
-    sel.close()
+    try:
+        while True:
+            start_connection(host, port, request)
+    except KeyboardInterrupt:
+        print("Client application shutting down.")
--- Application Message/app-server.py
+++ Application Message/app-server.py
@@ -1,48 +1,30 @@
 import sys
 import socket
-import selectors
-import types
-import traceback 
 import threading
 
 from libserver import Message
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept()
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
-    message = Message(sel, conn, addr)
-    sel.register(conn, selectors.EVENT_READ, data=message)
-
-sel = selectors.DefaultSelector()
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 65432))
-server.listen(10)
-
-def start_server():
-    try:
-        while True:
-            events = sel.select(timeout=1)
-            print(f"Selected events: {events}")
-            for key, mask in events:
-                if key.data is None:
-                    accept_wrapper(key.fileobj)
-                else:
-                    message = key.data
-                    try:
-                        message.process_events(mask)
-                    except Exception:
-                        print(
-                            f"Main: Error: Exception for {message.addr}:\n"
-                            f"{traceback.format_exc()}"
-                        )
-
-    except KeyboardInterrupt:
-        print("Server shutting down.")
-    finally:
-        sel.close()
+def accept_connections(server_socket):
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Accepted connection from {client_address}")
+        client_socket.setblocking(False)
+        message = Message(client_socket, client_address)
+        message.start()
 
 if __name__ == "__main__":
-    print("Server is running...")
-    start_server()
+    host = '127.0.0.1'
+    port = 65432
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(10)
+
+print(f"Server is running on {host}:{port}")
+
+try:
+    while True:
+        accept_connections(server_socket)
+except KeyboardInterrupt:
+        print("Server shutting down.")
+finally:
+        server_socket.close()
